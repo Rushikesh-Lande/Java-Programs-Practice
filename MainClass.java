b4ae82390f33993e55ca8d80c9df1ef6c6836c55
Rushikesh Lande <rushilande999@gmail.com>
2024-03-01 17:37:04 +0530

    New Programs

--- MainClass.java
+++ MainClass.java
@@ -4,10 +4,10 @@ class MainClass{
   public static void main(String[] args){
       /* int[] arr={ 12,12,54,5,4,5,5,5,4,752,-4,5,333,-4};
        // int[] i1 = {1,0,0, 2, 3,0, 4, 5, 4,0,-1};
-       int[] i2 = {3, 4, 5, 6, 7, 4};
+       //int[] i2 = {3, 4, 5, 6, 7, 4};
       
       //List<Integer> result=SeparateZero.sZero(i1);
-      System.out.print(MissingNoArray.mNo(8,new int[] {1, 4, 5, 3, 7, 8, 6}));
+    //  System.out.print(MissingNoArray.mNo(8,new int[] {1, 4, 5, 3, 7, 8, 6}));
 
        //for(int a:result){ System.out.print(" "+a);}
 
@@ -18,7 +18,8 @@ List<Integer> integerList = Arrays.asList(1, 4, 5, 3, 7, 8, 6);
 
      // CountOccurrenceInArray.cO(new int[] { 12,12,54,5,4,5,5,5,4,752,-4,5,333,-4});
 
-        int[] array= RemoveArrayDuplicates.rD(new int[] { 12,12,54,5,4,5,0,0,54});
-      for(int a:array){ System.out.print(" "+a);}
+      //  int[] array= RemoveArrayDuplicates.rD(new int[] { 12,12,54,5,4,5,0,0,54});
+    //  for(int a:array){ System.out.print(" "+a);}
+         PrintProgramExample1.pS(7);
 }
 }
---
+++ PrintProgramExample1.java
@@ -0,0 +1,22 @@
+// print the following 
+
+class PrintProgramExample1{
+
+     public static void pS(int n){
+
+        for(int i=1;i<=n;i++){
+ 
+            for(int j=1;j<=i;j++){
+             
+             System.out.print("*");
+
+               }
+
+             System.out.println();
+
+           }
+
+
+  }
+
+}
